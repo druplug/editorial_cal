1af972ef13dc99c5741cd4347066bd8e532a2992
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2016-11-07 12:27:40 +0530

    drag and drop external events

--- js/editorial_cal.js
+++ js/editorial_cal.js
@@ -1,6 +1,24 @@
 (function ($) {
     Drupal.behaviors.editorialCal = {
 	attach: function (context, settings) {
+
+	    $('div.view-unscheduled-posts .fc-event').each(function() {
+
+		// store data so the calendar knows to render an event upon drop
+		$(this).data('event', {
+		    title: $.trim($(this).text()), // use the element's text as the event title
+		    stick: true // maintain when user navigates (see docs on the renderEvent method)
+		});
+
+		// make the event draggable using jQuery UI
+		$(this).draggable({
+		    zIndex: 9999,
+		    revert: true,      // will cause the event to go back to its
+		    revertDuration: 0  //  original position after the drag
+		});
+
+	    });
+
 	    function get_calendar_height() {
 		return $(window).height() - 63;
 	    }
@@ -27,6 +45,10 @@
 		},
 		eventDrop:  function( event, delta) {
 		    console.log(event.title + " was dropped on " + event.start.format());
+		},
+		drop: function(date) {
+		    $(this).remove();
+		    console.log($(this).find('span.post-title').text() + " dropped on: " + date.format());
 		},
 		events: {
 		    url: 'https://gist.githubusercontent.com/badri/efd6492ecb436bee19cd1aeec84188a7/raw/62812734d2b05e1239fcbca39fcb317cc6c770cc/events.json',
